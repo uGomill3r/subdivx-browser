bfa7e3966ea1da00dba91196820204f822c472c8
devops <evoluxiperu@gmail.com>
2026-03-20 01:49:21 -0500

    fix: se corrige archivos urls.py editados por error.

--- browser/urls.py
+++ browser/urls.py
@@ -5,6 +5,7 @@ app_name = "browser"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("folders/", views.folder_list, name="folder_list"),
     path("folder/<str:folder_name>/", views.folder_detail, name="folder_detail"),
     path("folder/<str:folder_name>/search/", views.search_subtitles_view, name="search_subtitles"),
     path("folder/<str:folder_name>/download/", views.download_and_save, name="download_and_save"),
